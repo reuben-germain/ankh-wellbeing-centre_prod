961fddf4112892e73f8bd97ec3a99bc27bb512a4
Reuben Germain <reuben.germain@soprasteria.com>
2024-06-13 14:31:10 +0100

    formatted code & added a border for mobile devices

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.getElementById('hamburger').addEventListener('click', function() {
+document.getElementById('hamburger').addEventListener('click', function () {
     var navLinks = document.getElementById('navLinks');
     if (navLinks.style.display === 'flex') {
         navLinks.style.display = 'none';
